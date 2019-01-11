c12e6b73058032f62969b61b12018f6d657336e2
[akshephard] <akshephard@ucdavis.edu>
2019-01-11 15:37:02 -0800

    Added timezone conversion to utc

--- EF_final_bak.py
+++ EF_final_bak.py
@@ -361,7 +361,8 @@ class myThread1(threading.Thread):
         while True:
             print("Entering thread 1")
             ser1 = self.ser.readline()
-            dt_object = datetime.now()
+            #dt_object = datetime.now()
+            dt_object = datetime.utcnow()
             #dt_object = utc_to_local(dt_object)
             time_str1 = dt_object.strftime('%H:%M:%S')
 
@@ -395,7 +396,7 @@ class myThread1(threading.Thread):
                 new_time = area_time(self.area_temp,dt_object)
                 all_area.area_time_abcd1.append(new_time)
                 print("The bc_abcd1 is: "+ str(bc_abcd1))
-                test_client.write_json(json,'truck_test')
+                test_client.write_json(json,'truck_time_5')
 
             except (ValueError,IndexError) as e:
                 print("issue in push?")
@@ -1458,6 +1459,7 @@ all_area=area_container()
 conf_file = "local_server.yaml"
 test_client = Influx_Dataframe_Client(conf_file,'DB_config')
 thread1=myThread1(serial1,all_area)
+'''
 thread2=myThread2(serial2,all_area)
 thread3=myThread3(serial3,all_area)
 thread4=myThread4(serial4,all_area)
@@ -1467,9 +1469,11 @@ thread7=myThread7(serial7,all_area)
 thread8=myThread8(serial8,all_area)
 thread9=myThread9(serial9,all_area)
 thread10=myThread10(serial10,all_area)
+'''
 #thread11=myThread11(serial11,all_area)
 area_thread=areaThread(all_area)
 thread1.start()
+'''
 thread2.start()
 thread3.start()
 thread4.start()
@@ -1479,5 +1483,6 @@ thread7.start()
 thread8.start()
 thread9.start()
 thread10.start()
+'''
 #thread11.start()
 area_thread.start()
