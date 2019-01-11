74439a968d1ca4edc29055e06a62d548f9c78f40
[akshephard] <akshephard@ucdavis.edu>
2019-01-11 11:59:04 -0800

    Added all threads back in and switched back to how time was previously

--- EF_final_bak.py
+++ EF_final_bak.py
@@ -339,7 +339,7 @@ class myThread1(threading.Thread):
             print("Entering thread 1")
             ser1 = self.ser.readline()
             dt_object = datetime.now()
-            dt_object = utc_to_local(dt_object)
+            #dt_object = utc_to_local(dt_object)
             time_str1 = dt_object.strftime('%H:%M:%S')
 
             values_abcd1 = ser1.split('\n')[0].split(',')
@@ -372,7 +372,7 @@ class myThread1(threading.Thread):
                 new_time = area_time(self.area_temp,dt_object)
                 all_area.area_time_abcd1.append(new_time)
                 print("The bc_abcd1 is: "+ str(bc_abcd1))
-                test_client.write_json(json,'truck_time_3')
+                test_client.write_json(json,'truck_test')
 
             except (ValueError,IndexError) as e:
                 print("issue in push?")
@@ -461,7 +461,7 @@ class myThread2(threading.Thread):
             print("Entering thread2")
             ser2 = self.ser.readline()
             dt_object = datetime.now()
-            dt_object = utc_to_local(dt_object)
+            #dt_object = utc_to_local(dt_object)
             time_str2 = dt_object.strftime('%H:%M:%S')
             values_ae16 = ser2.split('\n')[0].split(',')
 
@@ -1436,7 +1436,6 @@ all_area=area_container()
 conf_file = "local_server.yaml"
 test_client = Influx_Dataframe_Client(conf_file,'DB_config')
 thread1=myThread1(serial1,all_area)
-"""
 thread2=myThread2(serial2,all_area)
 thread3=myThread3(serial3,all_area)
 thread4=myThread4(serial4,all_area)
@@ -1446,11 +1445,9 @@ thread7=myThread7(serial7,all_area)
 thread8=myThread8(serial8,all_area)
 thread9=myThread9(serial9,all_area)
 thread10=myThread10(serial10,all_area)
-"""
 #thread11=myThread11(serial11,all_area)
 area_thread=areaThread(all_area)
 thread1.start()
-"""
 thread2.start()
 thread3.start()
 thread4.start()
@@ -1460,6 +1457,5 @@ thread7.start()
 thread8.start()
 thread9.start()
 thread10.start()
-"""
 #thread11.start()
 area_thread.start()
