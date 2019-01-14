e809bf178084179e35c6465064394dbbb39abc28
[akshephard] <akshephard@ucdavis.edu>
2019-01-14 15:46:48 -0800

    Added test for signal handler

---
+++ EF_final_sig.py
@@ -0,0 +1,59 @@
+import serial
+#from time import time
+import time
+import threading
+from Influx_Dataframe_Client import Influx_Dataframe_Client
+import datetime as dt
+from datetime import datetime
+#from datetime import timedelta
+import numpy as np
+#import matplotlib.pyplot as plt
+#import matplotlib.animation as animation
+import os, sys, csv, re, math
+import pytz
+
+local_tz = pytz.timezone('Etc/GMT+8') # use your local timezone name here
+stop_requested = False
+
+def sig_handler(signum, frame):
+    sys.stdout.write("handling signal: %s\n" % signum)
+    sys.stdout.flush()
+
+    global stop_requested
+    stop_requested = True
+
+def utc_to_local(utc_dt):
+    local_dt = utc_dt.replace(tzinfo=pytz.utc).astimezone(local_tz)
+    return local_tz.normalize(local_dt) # .normalize might be unnecessary
+
+
+class myThread1(threading.Thread):
+
+    def __init__(self, ser,all_area):
+        threading.Thread.__init__(self)
+        self.ser = ser
+        self.N1 = 20
+
+        # Timestamps for all data
+        self.xs_abcd1 = []
+
+    def run(self):
+        i = 0
+        #while i < 240:
+
+        while True:
+            print("Continuing thread 1")
+            time.sleep(5)
+
+
+
+
+signal.signal(signal.SIGTERM, sig_handler)
+signal.signal(signal.SIGINT, sig_handler)
+
+all_area=area_container()
+conf_file = "local_server.yaml"
+test_client = Influx_Dataframe_Client(conf_file,'DB_config')
+thread1=myThread1(serial1,all_area)
+
+thread1.start()
