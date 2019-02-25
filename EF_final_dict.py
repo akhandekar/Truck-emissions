1b652d8fdbc61db719b02218402d15f8611f1fac
[akshephard] <akshephard@ucdavis.edu>
2019-02-25 11:22:35 -0800

    SHould have correct temp and pressure values

--- EF_final_dict.py
+++ EF_final_dict.py
@@ -970,11 +970,11 @@ class LI7000_Instrument(CO2_Sensor):
             print(values_li7000)
             co2_values.insert(0,float(values_li7000[2])) # CO2 value
             co2_values.insert(1,time_now)
-            co2_values.insert(2,float(values_li7000[5])) # Temp
-            co2_values.insert(3,float(values_li7000[4])) # Pressure
+            co2_values.insert(2,float(values_li7000[6])) # Temp
+            co2_values.insert(3,float(values_li7000[5])) # Pressure
             # Save most recent temperature and pressure from Li7000 instrument
-            self.all_peaks.current_li7000_temp = float(values_li7000[5])
-            self.all_peaks.current_li7000_pressure = float(values_li7000[4])
+            self.all_peaks.current_li7000_temp = float(values_li7000[6])
+            self.all_peaks.current_li7000_pressure = float(values_li7000[5])
             #print(co2_values)
         except (ValueError,IndexError) as e:
             print("li7000 index failure")
