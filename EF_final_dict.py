31ad0a0a65ec5ad001268cf6018f19e3aac9c9da
[akshephard] <akshephard@ucdavis.edu>
2019-02-27 18:03:24 -0800

    Added debugging for window

--- EF_final_dict.py
+++ EF_final_dict.py
@@ -297,7 +297,7 @@ class Peak_Container:
         #print("The amount of co2 peaks for " + co2_device + "is" + str(co2_peak_amt))
         time_now=int(time.time()*1000000000)
         for x in range(co2_peak_amt):
-            if (time_now > (self.co2_peaks[co2_device][x].end_time + 30*1000000000):
+            if (time_now > (self.co2_peaks[co2_device][x].end_time + 30*1000000000)):
                 for device,value in self.co2_peaks[co2_device][x].device_bc.items():
                     if (value == False):
                         print(device)
